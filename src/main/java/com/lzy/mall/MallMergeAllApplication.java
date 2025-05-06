2b11337647da7d444990007b63c9ff253b08cbbc
lzy <2557090008@qq.com>
2025-05-06 16:32:23 +0800

    初次提交，初始化mall项目

---
+++ src/main/java/com/lzy/mall/MallMergeAllApplication.java
@@ -0,0 +1,13 @@
+package com.lzy.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMergeAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallMergeAllApplication.class, args);
+    }
+
+}
